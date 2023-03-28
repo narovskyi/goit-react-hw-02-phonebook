337b6525660a978987dcd74c46c065e6d50ae538
Oleksii Narovskyi <o.narovskyi@gmail.com>
2023-03-28 21:13:12 +0300

    do some changes

--- src/components/UserAddForm/UserAddForm.jsx
+++ src/components/UserAddForm/UserAddForm.jsx
@@ -8,7 +8,7 @@ const initialValues = {
 
 const validationSchema = Yup.object().shape({
     name: Yup.string().required('Required'),
-    number: Yup.string().required('Required')
+    number: Yup.string().required('Required').min(4, 'Number is too short!')
 });
 
 const UserAddForm = ({addContact }) => (
@@ -23,6 +23,8 @@ const UserAddForm = ({addContact }) => (
                     placeholder="Name"
                     type="text"
                     name="name"
+                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                 />
                 <ErrorMessage component='div' name="name" />
                 <br/>
@@ -30,6 +32,8 @@ const UserAddForm = ({addContact }) => (
                     placeholder="Number"
                     type="tel"
                     name="number"
+                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                 />
                 <ErrorMessage
                     name="number"
